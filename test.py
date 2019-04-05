ad233abb08217879489b96b773d1390962a24318
Yash Rane <yash_rane@umail.ucsb.edu>
2019-04-05 10:40:19 -0700

    cleaned up notes in test.py

--- test.py
+++ test.py
@@ -66,10 +66,6 @@ test(df, rf)
 #ACF and PACF plots
 #sns.barplot(x=list(range(1,32)), y=acf(df['ASP'])[1:32])
 #sns.barplot(x=list(range(1,32)), y=pacf(df['ASP'])[1:32])
-"""
-These plots indicate that the ASPFWR5 likely follows an AR(1) model, so we
-only need to look at values from one time step beforehand
-"""
 
 
 #Correlations
@@ -87,40 +83,14 @@ ax.set_xticklabels(ax.get_xticklabels(),rotation=30)
 
 # NOTES
 '''
-Current Model: SAR(1, ?) x (?,?)
--need to difference out to acheive stationarity
-    -use hyp test to check before and after differencing bc too much data for accurate visual check
--need to account for seasonality somehow
--VAR models suddenly looking a lot more promising
--can i test for seasonality somehow?
-    -periodogram
+-no apparant seasonality
+-ASPFWR5 follows a rough AR(1) model
 
-
-
-this data feels dirty
-first half of ASP is 0? doesnt sound right
-    -do other metrics follow this pattern?
-
-- Cant produce any meaningful models until differenced and const var!
-    -how to difference when more vars than just Y? difference those too or leave them be? *****
-    -multivariate time series analysis
-    -when looking at performace metrics, do i look at the differenced or undifferenced data?
+need to try:
+    VAR model
+    dimensionality reduction with PCA
     
--BIC recommends lower order models than AIC?
-    -figure out how the hell these actually work
-    -also wtf is granger causality
-        -two vars are granger causal if they have some correlation through time
 
-honestly a fucking LSTM would be easier at this point
-    -this might actually be a good idea
-    -would still need to figure out how to difference    
-    
-VAR models are good apparantly
-    
-0. look at the covariance matrix! these R2 values are too good
-1. find which variables depend on time (should be all but ive been surprised before)
-2. difference those vars until stationary? 
-    -do we only care about stationarity for the response variable?
-    -why do i do this to myself?
-
+RNN with minute level data?
+ - look at Calvin's stock price predictor
 '''
