cf052ccc387509d2e4cd7feda66532278d18fa18
shaiyon <shaiyon@ucsb.edu>
2019-04-07 23:34:44 -0700

    Adjusted for needed use case

--- load.py
+++ load.py
@@ -2,36 +2,39 @@
 """
 Created on Thu Apr  4 22:18:38 2019
     
-@author: shaiyon
+@authors: shaiyon, yashr
 """
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
-def load(filepath='dataset.csv', preprocess=True, pred_days=1):
+def load(filepath='dataset.csv', preprocess=True):
     # Load data from csv
     df = pd.read_csv(filepath, index_col=0, parse_dates=True)
     
-    # Set up target as ASPFWR5 in pred_days days in the future
-    target = "ASPFWR5_{}DAY".format(pred_days)
-    df[target] = df["ASPFWR5"].shift(-pred_days, axis=0)
-    
+    # Drop rows
+    df.dropna(axis=0, inplace=True)
+
     # Preprocess data for modeling 
     if preprocess:          
-                         
-        # Drop rows
-        df.dropna(axis=0, inplace=True)
-        
+                                 
         # Separate features and targets
-        X = df.drop(target, axis=1)
-        y = df[target]
+        X = df.drop('ASPFWR5', axis=1)
+        y = df['ASPFWR5']
         
         # Standardize data
         X = StandardScaler().fit_transform(X)
             
         # Dimensionality reduction with principal component analysis 
-        X = PCA(n_components = 30).fit_transform(X)
-            
-        return X, y
-    
+        X = pd.DataFrame(PCA(n_components = 30).fit_transform(X))
+        
+        # Set index to datetime
+        X.reindex(y.index)        
+        df = pd.DataFrame(X.values, index=y.index)
+        # Merge X and y
+        df['ASPFWR5'] = y 
+ 
     return df
+
+filepath = "C:\\Users\\shaiyon\\Documents\\Datasets\\S&P 500 Prediction\\dataset.csv"
+df = load(filepath, preprocess=True)
