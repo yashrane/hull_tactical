5f1d908b50e4c56764a9f88574104fe6dd9d6d12
shaiyon <shaiyon@ucsb.edu>
2019-04-07 21:31:10 -0700

    Improved modularity and added PCA

--- load.py
+++ load.py
@@ -1,44 +1,37 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Apr  4 22:18:38 2019
-
+    
 @author: shaiyon
 """
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA 
+from sklearn.decomposition import PCA
 
-
-def load(filepath='dataset.csv', standardize=True, pred_time=1):
+def load(filepath='dataset.csv', preprocess=True, pred_days=1):
+    # Load data from csv
     df = pd.read_csv(filepath, index_col=0, parse_dates=True)
-    X, y = preprocess(df, standardize, pred_time)
-    return X, y
     
-
-
-def preprocess(df, standardize, pred_time):
+    # Set up target as ASPFWR5 in pred_days days in the future
+    target = "ASPFWR5_{}DAY".format(pred_days)
+    df[target] = df["ASPFWR5"].shift(-pred_days, axis=0)
     
-    cols = df.columns
-    
-    # Set up target as ASPFWR5 in pred_time days in the future
-    pred_name = "ASPFWR5_{}DAY".format(pred_time)
-    df[pred_name] = df["ASPFWR5"].shift(pred_time, axis=0)
-                 
-    # Drop rows
-    df.dropna(axis=0, inplace=True)
-    
-    # Separate features and targets 
-    X = df.drop(pred_name, axis=1)
-    y = df[pred_name]
-    
-    if standardize:
-        # Standardize data
-        X = pd.DataFrame((StandardScaler().fit_transform(X, y)), columns=cols)
-    
-    # Principal component analysis   
-    #pca = PCA(n_components = 10)
-    #pca.fit(df)
+    # Preprocess data for modeling 
+    if preprocess:          
+                         
+        # Drop rows
+        df.dropna(axis=0, inplace=True)
         
-    return X, y
-
-
+        # Separate features and targets
+        X = df.drop(target, axis=1)
+        y = df[target]
+        
+        # Standardize data
+        X = StandardScaler().fit_transform(X)
+            
+        # Dimensionality reduction with principal component analysis 
+        X = PCA(n_components = 30).fit_transform(X)
+            
+        return X, y
+    
+    return df
